97d399bd9e904411d79d93fea277f87902f6362c
chenfuyuan <chenfuyuan0713@163.com>
2022-01-24 07:45:08 +0800

    feat(index): 完成 首页-快速挂机功能

--- afk/dayTask/index.lua
+++ afk/dayTask/index.lua
@@ -19,9 +19,25 @@ COLLECT_CANCEL_X = 237
 COLLECT_CANCEL_Y = 908
 COLLECT_NUM = 2
 
+--快速挂机
+ONHOOK_X = 619
+ONHOOK_Y = 1093
+CHECK_ONHOOK_NO_NEED_X=417
+CHECK_ONHOOK_NO_NEED_Y=837
+CHEKC_ONHOOK_NO_NEED_COLOR=0xf0e8ff
+ONHOOK_CONFIRM_X = 452
+ONHOOK_CONFIRM_Y =850
+ONHOOK_CANCEL_X = 259
+ONHOOK_CANCEL_Y =842
+ONHOOK_CANCEL_BLACK_COLOR = 0x39231c
+
 function index.run()
     --收集战利品
-    index.collect()
+    --index.collect()
+    
+    --快速挂机
+    index.quickOnHook()
+    
 end
 
 --领取战利品
@@ -37,6 +53,30 @@ function index.collect()
         --等待
         stepEnd()
     end
+    stepEnd()
+end
+
+--快速挂机
+function index.quickOnHook()
+    --点击快速挂机
+    myClick(ONHOOK_X,ONHOOK_Y);
+    --判断是否需要钻石 如果需要钻石则代表已经使用免费次数
+    --[[if (myFindColor(CHECK_ONHOOK_NO_NEED_X,CHECK_ONHOOK_NO_NEED_Y,CHEKC_ONHOOK_NO_NEED_COLOR)) then
+        --点击关闭
+        myClick(ONHOOK_CANCEL_X,ONHOOK_CANCEL_Y)
+        return;
+    end--]]
+    
+    --点击确认
+    myClick(ONHOOK_CONFIRM_X,ONHOOK_CONFIRM_Y)
+    
+    --黑屏点击任意键
+    if(myFindColor(ONHOOK_CANCEL_X,ONHOOK_CANCEL_Y,ONHOOK_CANCEL_BLACK_COLOR)) then
+        myClick(ONHOOK_CANCEL_X,ONHOOK_CANCEL_Y)
+    end    
+    --点击关闭
+    myClick(ONHOOK_CANCEL_X,ONHOOK_CANCEL_Y)
+    
     stepEnd()
 end
 
